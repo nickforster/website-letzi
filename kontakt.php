0629fea0fe55f47e4e5015830aaa1bf10c0ffbf7
Nick Forster <Forstern@bzz.ch>
2021-12-22 14:32:41 +0100

    files umbenant und geordnet

--- kontakt.php
+++ kontakt.php
@@ -4,7 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Kontaktformular | Zunft zur Letzi</title>
+
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/footer.css">
+
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="js/header.js" defer></script>
 </head>
 <body>
     <header>
@@ -16,7 +25,9 @@
         </script>
     </header>
     <main>
-        <!-- Kontaktformular -->
+        <?php
+        
+        ?>
     </main>
     <footer>
         <div id="footer-placeholder"></div>
